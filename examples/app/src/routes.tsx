64da8c29c443e297a0b8dcaf7e303aadba9a7794
toyamarinyon <toyamarinyon@gmail.com>
2023-03-01 02:01:24 +0900

    update example

--- examples/app/src/routes.tsx
+++ examples/app/src/routes.tsx
@@ -1,6 +1,11 @@
 import { Hello } from "./pages/Hello";
 import { World } from "./pages/World";
-import { createRoutes } from "raula";
-export const routes = createRoutes()
-  .add("/", <Hello />)
-  .add("/world", <World />);
+import { createRouter } from "raula";
+export const appRouter = createRouter()
+  .add("/", () => <Hello />)
+  .add("/world", () => <World />);
+
+type AppRouter = typeof appRouter;
+declare module "raula" {
+  interface Router extends AppRouter {}
+}
